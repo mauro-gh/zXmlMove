6ec2873154bb6cb4b4e59da110d744314d4182bd
Mauro <mauro.carta.76@gmail.com>
2024-03-18 01:15:30 +0100

    Versione iniziale

---
+++ Engine.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zXmlMove
+{
+    public class Engine
+    {
+        public string FileElenco { get; set; } = string.Empty;
+        public string XmlMoveFolder { get; set; } = string.Empty;
+        public string XmlPath { get; set; } = string.Empty;
+
+        public void XmlMove(){
+
+            try
+            {
+                string fromPath = Environment.CurrentDirectory;
+                string toPath = Path.Combine(fromPath, XmlMoveFolder);
+                string[] fileTxt;
+
+
+                if (!Path.Exists(toPath))
+                {
+                    Directory.CreateDirectory(toPath);
+                }
+
+                if (!File.Exists(FileElenco))
+                {
+                    throw new Exception($"File inesistente: {FileElenco}");
+                }
+
+                fileTxt = File.ReadAllLines(FileElenco);
+                Console.WriteLine($"Totale files da spostare: {fileTxt.Length}");
+                
+
+
+                
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine($"Eccezione: {ex.Message}");
+                if (ex.InnerException != null)
+                    Console.WriteLine($"Dettaglio: {ex.InnerException.Message}");
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using zXmlMove;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args == null || args.Length != 2 )
+        {
+            Console.WriteLine("Parametri:");
+            Console.WriteLine("FileTXT CartellaXMLdestinazione");
+            Console.WriteLine("Es: dotnet zXmlMove.dll input2023.txt 2023xml");
+            return;
+        }
+
+        int totArgs = args.Length;
+        Console.WriteLine($"Totale parametri: {totArgs}");
+
+        Engine eng = new Engine();
+        eng.FileElenco = args[0];
+        eng.XmlMoveFolder = args[1];
+
+        Console.WriteLine($"FileTXT: {eng.FileElenco}");
+        Console.WriteLine($"CartellaXMLdestinazione: {eng.XmlMoveFolder}");
+
+        eng.XmlMove();
+    }
+}
